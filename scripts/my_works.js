41df7effcec42a1fa68f76b8ce3036eec9793b9e
DarCkly666 <lessombremonsieur@hotmail.fr>
2022-05-12 21:02:07 -0400

    Version 3

--- scripts/my_works.js
+++ scripts/my_works.js
@@ -1,39 +1,46 @@
-'use strict';
+"use strict";
 
 let myWorks = [];
 const worksContainer = document.querySelector(".my_work_container");
 
 fetch("../assets/my_work.json")
-.then(data => data.json())
-.then(response => {
-  response.forEach(work => {
-    worksContainer.appendChild(createItemWork(work));
+  .then((data) => data.json())
+  .then((response) => {
+    response.forEach((work) => {
+      worksContainer.appendChild(createItemWork(work));
+    });
+  })
+  .catch((error) => {
+    console.error(error);
   });
-})
-.catch(error => {console.error(error)});
 
 const gitrepoButton = (url_github) => `<button class="btn_view gh">
 <a href="${url_github}" target="_blank">View GitHub Repo</a>
+</button>`;
+
+const viewRepoButton = (url) => `<button class="btn_view">
+<a href="${url}" target="_blank">View</a>
 </button>`;
 
 const createItemWork = (item) => {
   const contWork = document.createElement("div");
   contWork.className = "my_work_item";
   contWork.style.backgroundImage = `url("${item.image}")`;
+
+  const viewButton = (url) => (url.length !== 0 ? viewRepoButton(url) : "");
+
   const gitButton = (url_github) => {
     return url_github.length !== 0 ? gitrepoButton(url_github) : "";
-  }
-  if (!item.construction){
+  };
+  if (!item.construction) {
     contWork.innerHTML = `
     <div class="work_title_container">
       <p class="work_title">${item.title}</p>
-      <button class="btn_view">
-        <a href="${item.url}" target="_blank">View</a>
-      </button>
+      ${viewButton(item.url)}
       ${gitButton(item.url_github)}
     </div>
   `;
-  }else{
+  } else {
     contWork.innerHTML = `
       <div class="work_title_container">
         <p class="work_title">${item.title}</p>
@@ -42,9 +49,11 @@ const createItemWork = (item) => {
     `;
   }
   return contWork;
-}
+};
 
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+});
